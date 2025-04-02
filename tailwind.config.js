a5871394efb40d57edff63cb07db4e1904c71574
ZugastiKevin <bearded-developer@protonmail.com>
2025-04-02 13:27:42 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,js}","*.html"],
+  theme: {
+    extend: {
+      colors: {
+      },
+      fontFamily: {
+        roboto: ['Roboto Regular'],
+      }
+    },
+  },
+  plugins: [],
+}
